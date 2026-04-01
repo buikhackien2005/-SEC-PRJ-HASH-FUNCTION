28f81533af6846c8f4eddd049af5d6be164bce07
Hungycth <caoquanghung05@gmail.com>
2026-04-01 23:59:40 +0700

    Hung update code padding parsing

--- hash_padding_parsing.cpp
+++ hash_padding_parsing.cpp
@@ -2,14 +2,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 64 bits to 16 bytes
 string hex64(u64 x) {
     stringstream ss;
     ss << hex << nouppercase << setfill('0') << setw(16) << x;
     return ss.str();
 }
 
-// Hàm in ra các giá trị trung gian
 void print_bytes_hex(const vector<u8>& data, ostream& out, size_t perLine = 16) {
     for (size_t i = 0; i < data.size(); ++i) {
         out << hex << nouppercase << setfill('0') << setw(2)
@@ -20,12 +18,21 @@ void print_bytes_hex(const vector<u8>& data, ostream& out, size_t perLine = 16) 
     out << dec;
 }
 
-// I, Padding & Parsing function (Skeleton)
+// I, Padding & Parsing function (Core Padding)
 vector<u8> PaddingParsing_SHA512(const string& msg) {
-    // Sao chép nội dung chuỗi vào vector byte
     vector<u8> data(msg.begin(), msg.end());
 
-    // TODO: Implement padding logic here
+    // Thêm 1 bit '1' và 7 bit '0' => tương đương byte 0x80
+    data.push_back(0x80);
+
+    // Thêm các byte 0x00 cho đến khi:
+    // (độ dài hiện tại + 16 byte độ dài cuối) chia hết cho 128 byte
+    // vì mỗi block SHA-512 có kích thước 1024 bit = 128 byte
+    while ((data.size() + 16) % 128 != 0) {
+        data.push_back(0x00);
+    }
+
+    // TODO: Append original message length
 
     return data;
 }
